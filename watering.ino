0e6ca522a8606689c2fe4b956e8c7b08bd8c3037
Guoliang <guoliang.li@assurly.com>
2022-07-10 01:24:28 +0200

    timer updated

--- watering.ino
+++ watering.ino
@@ -1,14 +1,16 @@
 int plantPin = 7;
-int timeDelayOn = 1000 * 60 * 10;
-int timeDelayOff = 1000 * 60 * 60 * 3;
+int timeDelayOn = 60 * 10;
+int timeDelayOff = 60 * 60 * 3;
+// int timeDelayOn = 1000 * 60 * 60 * 3;
+// int timeDelayOff = 1000 * 60 * 20;
 void setup()
 {
   Serial.begin(9600);
   //   pinMode(A5, INPUT);
   pinMode(plantPin, OUTPUT);
-  while (!Serial)
-    ;
-  Serial.println("start ");
+  // while (!Serial)
+  //   ;
+  // Serial.println("start ");
 }
 
 void loop()
@@ -17,11 +19,18 @@ void loop()
   //   if(SensorValue >= 500)
   //   {
   digitalWrite(plantPin, LOW);
-  Serial.print("LOW-ON ");
-  delay(timeDelayOn);
-  Serial.print("HIGH-OFF ");
+  // Serial.print("LOW-ON ");
+  for (int count = 0; count < timeDelayOn; count++)
+  {
+    delay(1000);
+  }
+  // delay(timeDelayOn);
+  // Serial.print("HIGH-OFF ");
   digitalWrite(plantPin, HIGH);
-  delay(timeDelayOff);
+  for (int count = 0; count < timeDelayOff; count++)
+  {
+    delay(1000);
+  }
   //   }
   //   delay(50);
 }
