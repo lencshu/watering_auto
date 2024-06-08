814d0ec56c3a69b2a9d624708015016389d188df
Guoliang <guoliang.li@assurly.com>
2024-06-08 19:58:27 +0200

    updated with Potentiometer

--- watering/watering.ino
+++ watering/watering.ino
@@ -1,15 +1,38 @@
 const int plantPin = 7;
+const int potPin = A1; // 滑动变阻器连接的接口
 const int timeDelayUnit = 35; // 单次浇水时间，单位：秒
 const int timeDelayOff4h = 60 * 60 * 4; // 浇水间隔时间4小时，单位：秒
+
+int wateringTimes = 0; // 一天浇水次数
+int previousPotValue = -1; // 上一次读取的滑动变阻器的值
 
 void setup() {
   Serial.begin(9600);
   pinMode(plantPin, OUTPUT);
+  pinMode(potPin, INPUT);
+  updateWateringTimes(); // 初始化浇水次数
 }
 
-void delaySeconds(int seconds) {
-  Serial.println(nbSecond);
+void updateWateringTimes() {
+  int potValue = analogRead(potPin);
+  int actualPotValue = 1023 - potValue;
+  wateringTimes = (actualPotValue / 100) % 10; // 取百位数作为浇水次数
+  Serial.print("Updated Watering Times: ");
+  Serial.println(wateringTimes);
+}
+
+void delaySecondsWithPotCheck(int seconds) {
   for (int i = 0; i < seconds; i++) {
+    int potValue = analogRead(potPin);
+    int actualPotValue = 1023 - potValue;
+    Serial.print("Current Potentiometer Value: ");
+    Serial.println(actualPotValue);
+    if (actualPotValue / 100 != previousPotValue / 100) { // 检查百位数是否变化
+      previousPotValue = actualPotValue;
+      Serial.println("Potentiometer value changed, aborting watering.");
+      updateWateringTimes();
+      return; // 终止当前浇水
+    }
     delay(1000); // 延时1秒
   }
 }
@@ -17,15 +40,15 @@ void delaySeconds(int seconds) {
 void wateringCycle() {
   digitalWrite(plantPin, LOW);
   Serial.println("LOW-ON");
-  delaySeconds(timeDelayUnit);
-
+  delaySecondsWithPotCheck(timeDelayUnit);
+  
   digitalWrite(plantPin, HIGH);
   Serial.println("HIGH-OFF");
-  delaySeconds(timeDelayUnit);
+  delaySecondsWithPotCheck(timeDelayUnit);
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < wateringTimes; i++) {
     wateringCycle();
   }
 
@@ -34,6 +57,6 @@ void loop() {
 
   // 总共延时20小时，分为5个4小时
   for (int i = 0; i < 5; i++) {
-    delaySeconds(timeDelayOff4h);
+    delaySecondsWithPotCheck(timeDelayOff4h);
   }
 }
