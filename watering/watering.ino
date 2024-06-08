c319dd0cf4d5fc50d76e3bf7d6372827ed83482a
Guoliang <guoliang.li@assurly.com>
2024-06-08 19:10:05 +0200

    optimised

--- watering/watering.ino
+++ watering/watering.ino
@@ -1,57 +1,39 @@
-int plantPin = 7;
-// 连续浇水时间
-int timeDelayUnit = 35 * 1;
-// 浇水间隔时间20h
-int timeDelayOff4h = 60 * 60 * 4;
-// int timeDelayOn = 1000 * 60 * 60 * 3;
-// int timeDelayOff = 1000 * 60 * 20;
-void setup()
-{
+const int plantPin = 7;
+const int timeDelayUnit = 35; // 单次浇水时间，单位：秒
+const int timeDelayOff4h = 60 * 60 * 4; // 浇水间隔时间4小时，单位：秒
+
+void setup() {
   Serial.begin(9600);
-  //   pinMode(A5, INPUT);
   pinMode(plantPin, OUTPUT);
-  // while (!Serial)
-  //   ;
-  // Serial.println("start ");
 }
 
-void delay1s(int nbSecond)
-{
+void delaySeconds(int seconds) {
   Serial.println(nbSecond);
-  // for (int count = 0; count < timeDelayOn; count++)
-  for (int count = 0; count < nbSecond; count++)
-  {
-    delay(1000);
+  for (int i = 0; i < seconds; i++) {
+    delay(1000); // 延时1秒
   }
 }
 
-void wateringUnit()
-{
+void wateringCycle() {
   digitalWrite(plantPin, LOW);
   Serial.println("LOW-ON");
-  delay1s(timeDelayUnit);
+  delaySeconds(timeDelayUnit);
+
   digitalWrite(plantPin, HIGH);
   Serial.println("HIGH-OFF");
-  delay1s(timeDelayUnit);
+  delaySeconds(timeDelayUnit);
 }
 
-void loop()
-{
-  //   int SensorValue = analogRead(A5);
-  //   if(SensorValue >= 500)
-  //   {
-  wateringUnit();
-  wateringUnit();
-  wateringUnit();
-  wateringUnit();
-  // Serial.print("LOW-ON ");
-  // Serial.print("HIGH-OFF ");
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    wateringCycle();
+  }
+
+  digitalWrite(plantPin, HIGH); // 确保浇水结束后保持关闭
   Serial.println("HIGH-OFF");
-  digitalWrite(plantPin, HIGH);
-  delay1s(timeDelayOff4h);
-  delay1s(timeDelayOff4h);
-  delay1s(timeDelayOff4h);
-  delay1s(timeDelayOff4h);
-  delay1s(timeDelayOff4h);
-  //   }
+
+  // 总共延时20小时，分为5个4小时
+  for (int i = 0; i < 5; i++) {
+    delaySeconds(timeDelayOff4h);
+  }
 }
