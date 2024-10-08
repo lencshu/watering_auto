dc5841a66fd7f301a96ee941489fa5a363da4350
Guoliang <guoliang.li@assurly.com>
2024-10-08 16:39:49 +0200

    cmt fixed

--- watering/watering.ino
+++ watering/watering.ino
@@ -49,7 +49,7 @@ void loop()
   // Serial.print("HIGH-OFF ");
   Serial.println("HIGH-OFF");
   digitalWrite(plantPin, HIGH);
-// 浇水间隔 4h * 3
+// 浇水间隔 4h * 6
   delayBySecond(timeDelayOff4h);
   delayBySecond(timeDelayOff4h);
   delayBySecond(timeDelayOff4h);
